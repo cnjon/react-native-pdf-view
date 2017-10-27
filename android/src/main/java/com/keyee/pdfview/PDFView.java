b7f4791138d599356e8ce99e061a4c87496e05f0
Pranav Raj Singh Chauhan <pranavchauhan01@gmail.com>
2017-10-27 12:07:05 +0530

    Update PDFView.java

--- android/src/main/java/com/keyee/pdfview/PDFView.java
+++ android/src/main/java/com/keyee/pdfview/PDFView.java
@@ -18,7 +18,6 @@ public class PDFView implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
