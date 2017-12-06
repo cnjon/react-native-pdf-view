36e12f60cbdf73f3676c2b4482015a07201e28f6
Noitidart <noitidart@gmail.com>
2017-12-06 15:30:47 -0800

    Update PDFView.android.js

--- PDFView.android.js
+++ PDFView.android.js
@@ -1,6 +1,7 @@
 'use strict';
-import React,{ Component, PropTypes } from 'react';
+import React,{ Component } from 'react';
 import { requireNativeComponent, View } from 'react-native';
+import PropTypes from 'prop-types';
 
 class PDFView extends Component {
   constructor(props) {
