35d27c1230f660e4be3c5cdbab50df72507b2be5
Pedro González-Santiago González <perico.gonzalezsantiago@gmail.com>
2018-07-12 13:22:25 +0200

    Update propTypes in iOs file.

--- PDFView.ios.js
+++ PDFView.ios.js
@@ -1,5 +1,6 @@
 'use strict';
-import React,{ Component, PropTypes } from 'react';
+import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
 import { requireNativeComponent, View } from 'react-native';
 
 class PDFView extends Component {
