c77c7c862fc4650d6aad6db283c696e43b766c4e
Nam Vo <nam.vo@employmenthero.com>
2021-02-08 11:00:37 +0700

    Display pdf view when onAttachedWindow

---
+++ android/src/main/java/com/keyee/pdfview/RCTPdfView.java
@@ -0,0 +1,30 @@
+package com.keyee.pdfview;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+public class RCTPdfView implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+    // ReactPackage deprecated `createJSModules` as of RN 0.47.0
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        List<ViewManager> modules = new ArrayList<>();
+        modules.add(new PDFViewManager(reactContext));
+        return modules;
+    }
+}
